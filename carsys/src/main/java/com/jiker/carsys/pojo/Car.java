44acf2956711535782fbb1c2f2b3cc28d3288808
swkf5n <yaoxi_yan@saic-gm.com>
2020-07-03 08:20:39 +0800

    啦啦啦

---
+++ carsys/src/main/java/com/jiker/carsys/pojo/Car.java
@@ -0,0 +1,40 @@
+package com.jiker.carsys.pojo;
+
+public class Car {
+    private Integer id;
+    private String brand;
+    private String type;
+    private Float price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
---
+++ carsys/src/main/java/com/jiker/carsys/pojo/CarAccessories.java
@@ -0,0 +1,49 @@
+package com.jiker.carsys.pojo;
+
+public class CarAccessories {
+    private Integer id;
+    private String brand;
+    private String accessoriesName;
+    private Boolean availability;
+    private Double price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getAccessoriesName() {
+        return accessoriesName;
+    }
+
+    public void setAccessoriesName(String accessoriesName) {
+        this.accessoriesName = accessoriesName;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
